c69da34f6ed0c963b827a89185af1db744856d59
photondragon <photondragon@163.com>
2015-10-27 21:29:20 +0800

    Initial Commit

---
+++ IDNPhotoView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IDNPhotoView
+//
+//  Created by photondragon on 15/10/27.
+//  Copyright © 2015年 iosdev.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
